efbfe41713bedb88011e4363f1815ad25b70ca80
milliro <mrosmoura@gmail.com>
2022-09-26 18:22:14 -0300

    Concluindo mais ou menos o footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../styles/Footer.css';
+
+function Footer() {
+  const history = useHistory();
+
+  return (
+    <footer data-testid="footer">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img
+          src={ drinkIcon }
+          data-testid="drinks-bottom-btn"
+          alt="Icone de bebida"
+        />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/meals') }
+      >
+        <img
+          src={ mealIcon }
+          data-testid="meals-bottom-btn"
+          alt="Icone de refeições"
+        />
+      </button>
+    </footer>
+  );
+}
+
+export default Footer;
