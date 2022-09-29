4f63a7172502627ad4f5599d7d8b8cb5b6c1b950
Thomas Boos Pegler <tboospegler@gmail.com>
2022-09-29 16:59:38 -0400

    css pagina perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import done from '../images/done.svg';
+import favorite from '../images/favorite.svg';
+import logout from '../images/logout.svg';
+import '../styles/Profile.css';
 
 function Profile() {
   const [emailStoraged, setEmailStoraged] = useState('');
@@ -20,28 +24,35 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" profile="true" search="false" />
-      <h4 data-testid="profile-email">{ emailStoraged }</h4>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/done-recipes') }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ handleLogout }
-      >
-        Logout
-      </button>
+      <section className="profile-container">
+        <h4 data-testid="profile-email">{ emailStoraged }</h4>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          <img src={ done } alt="" />
+          Done Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          <img src={ favorite } alt="" />
+          Favorite Recipes
+        </button>
+        <hr />
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          <img src={ logout } alt="" />
+          Logout
+        </button>
+      </section>
       <Footer />
     </div>
   );
